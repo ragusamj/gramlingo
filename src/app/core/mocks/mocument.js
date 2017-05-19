0963df3322b95661d81a5168210dc2ac0565fac1
erikbarke <e@barke.nu>
2017-05-19 16:46:42 +0200

    Mock body.addEventListener

--- src/app/core/mocks/mocument.js
+++ src/app/core/mocks/mocument.js
@@ -1,24 +1,43 @@
+const listeners = {};
+
 class Mocument {
+
     constructor() {
         this._elements = {};
         global.document = this;
     }
+
     mockElement(id, element) {
         this._elements[id] = element;
         return this;
     }
+
     mockElements(elements) {
         this._elements = elements;
         return this;
     }
+
+    createElement() {
+        //
+    }
+
     getElementById(id) {
         return this._elements[id];
     }
 }
 
 Mocument.prototype.body = {
-    addEventListener: () => {
-        //
+    addEventListener: (eventName, callback) => {
+        listeners[eventName] = listeners[eventName] || [];
+        listeners[eventName].push(callback);
+    },
+    fireEvent: (eventName) => {
+        let callbacks = listeners[eventName];
+        if(callbacks) {
+            callbacks.forEach((callback) => {
+                callback();
+            });
+        }
     }
 };
 

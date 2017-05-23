9038c257e7dafcb9df30e230e62ed476a3f72f25
erikbarke <e@barke.nu>
2017-05-23 11:41:36 +0200

    Split gzip functionality to separate file

--- json-minify.js
+++ json-minify.js
@@ -2,9 +2,7 @@ var fs = require("fs");
 var glob = require("glob");
 var mkdirp = require("mkdirp");
 var path = require("path");
-var zlib = require("zlib");
 
-var compressionLevel = zlib.Z_BEST_COMPRESSION || zlib.constants.Z_BEST_COMPRESSION;
 var outputDirectory = path.join(process.cwd(), process.argv[3]);
 
 glob(process.argv[2], {}, function (error, files) {
@@ -19,11 +17,7 @@ glob(process.argv[2], {}, function (error, files) {
                 }
                 else {
                     fs.writeFile(out, minified, function() {
-                        zlib.gzip(minified, { level: compressionLevel }, function(error, buffer) {
-                            fs.writeFile(out + ".gz", buffer, function() {
-                                //
-                            });
-                        });
+                        //
                     });
                 }
             });

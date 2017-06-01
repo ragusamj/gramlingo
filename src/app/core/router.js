02c18da4c77d9647dc3dd8d41d1ce165d0226089
erikbarke <e@barke.nu>
2017-06-01 21:31:08 +0200

    Not using underscore prefix for private stuff

--- src/app/core/router.js
+++ src/app/core/router.js
@@ -3,62 +3,62 @@ import Template from "./template";
 class Router {
 
     constructor(browserEvent, http, i18n, routes, placeholderElementId) {
-        this._browserEvent = browserEvent;
-        this._http = http;
-        this._i18n = i18n;
-        this._routes = routes;
-        this._placeholderElementId = placeholderElementId;
+        this.browserEvent = browserEvent;
+        this.http = http;
+        this.i18n = i18n;
+        this.routes = routes;
+        this.placeholderElementId = placeholderElementId;
         window.onhashchange = () => {
-            let newRouteKey = this._getRouteKeyFromAddressBar();
+            let newRouteKey = this.getRouteKeyFromAddressBar();
             Object.keys(routes).forEach((key) => {
-                if (newRouteKey === key && newRouteKey !== this._currentRouteKey) {
-                    this._setRoute(key, this._routes[key]);
+                if (newRouteKey === key && newRouteKey !== this.currentRouteKey) {
+                    this.setRoute(key, this.routes[key]);
                 }
             });
         };
         document.addEventListener("DOMContentLoaded", () => {
-            this._initialize();
+            this.initialize();
         });
     }
 
-    _initialize() {
-        let route = this._getRouteFromAddressBar() || this._getDefaultRoute();
+    initialize() {
+        let route = this.getRouteFromAddressBar() || this.getDefaultRoute();
         if (route) {
-            this._setRoute(route.key, route.data);
+            this.setRoute(route.key, route.data);
         }
     }
 
-    _getRouteFromAddressBar() {
-        let routeKey = this._getRouteKeyFromAddressBar();
-        return this._routes[routeKey] ?
-            { key: routeKey, data: this._routes[routeKey] } :
+    getRouteFromAddressBar() {
+        let routeKey = this.getRouteKeyFromAddressBar();
+        return this.routes[routeKey] ?
+            { key: routeKey, data: this.routes[routeKey] } :
             undefined;
     }
 
-    _getDefaultRoute() {
+    getDefaultRoute() {
         let route;
-        Object.keys(this._routes).forEach((key) => {
-            if(this._routes[key].isDefault) {
-                route = { key: key, data: this._routes[key] };
+        Object.keys(this.routes).forEach((key) => {
+            if(this.routes[key].isDefault) {
+                route = { key: key, data: this.routes[key] };
             }
         });
         return route;
     }
 
-    _getRouteKeyFromAddressBar() {
+    getRouteKeyFromAddressBar() {
         return window.location.hash.substring(1);
     }
 
-    _setRoute(routeKey, routeData) {
-        this._browserEvent.emit("route-change-start", routeKey);
-        this._http.getHTML(routeData.template, (html) => {
+    setRoute(routeKey, routeData) {
+        this.browserEvent.emit("route-change-start", routeKey);
+        this.http.getHTML(routeData.template, (html) => {
             let pageTemplate = new Template(html);
             let onDOMChanged = () => {
-                pageTemplate.replaceContent(this._placeholderElementId);
-                this._i18n.translateApplication();
-                this._browserEvent.emit("route-change-success", routeKey);
+                pageTemplate.replaceContent(this.placeholderElementId);
+                this.i18n.translateApplication();
+                this.browserEvent.emit("route-change-success", routeKey);
                 window.location.hash = routeKey;
-                this._currentRouteKey = routeKey;
+                this.currentRouteKey = routeKey;
             };
             setTimeout(() => {
                 routeData.page.load(pageTemplate, onDOMChanged);

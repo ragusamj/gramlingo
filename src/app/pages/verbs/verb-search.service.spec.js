50f3c94e5a3a3d9493d9f43d359b5c2539c892dc
erikbarke <e@barke.nu>
2017-06-08 02:47:14 +0200

    Unit tests for search service

--- src/app/pages/verbs/verb-search.service.spec.js
+++ src/app/pages/verbs/verb-search.service.spec.js
@@ -1,10 +1,162 @@
 import test from "tape";
 import VerbSearchService from "./verb-search.service";
 
-test("Verb search service should search", (t) => {
-    let verbs = [{ name: "Ir" }];
-    let service = new VerbSearchService(verbs);
-    //t.deepEqual(service.search("ir"), { matches: [ { index: 0, match: "ir", post: "", pre: "", weight: 100 } ], maxExceeded: false });
-    t.true(service.search("ir"));
+test("Verb search service should skip searching if term contains non-spanish alphabetic chars", (t) => {
+    let service = new VerbSearchService([{ name: "Ir" }]);
+    t.deepEqual(service.search("ir++"), { matches: [], maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service should give exact matches the highest weight", (t) => {
+    let service = new VerbSearchService([{ name: "Haber" }]);
+    t.deepEqual(service.search("haber"), {
+        matches: [
+            { index: 0, match: "haber", post: "", pre: "", source: "", weight: 100 },
+            { index: 0, match: "haber", post: "", pre: "", source: "haber", weight: 50 } // TODO: remove duplicates
+        ],
+        maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service should give matches from the beginning of words the second highest weight", (t) => {
+    let service = new VerbSearchService([{ name: "Abrir" }]);
+    t.deepEqual(service.search("ab"), {
+        matches: [
+            { index: 0, match: "ab", post: "rir", pre: "", source: "", weight: 90 }
+        ],
+        maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service should give matches inside of words a lower weight", (t) => {
+    let service = new VerbSearchService([{ name: "Abrir" }]);
+    t.deepEqual(service.search("ir"), {
+        matches: [
+            { index: 0, match: "ir", post: "", pre: "abr", source: "", weight: 77 }
+        ],
+        maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service should give phonetic matches the lowest weight", (t) => {
+    let service = new VerbSearchService([{ name: "Haber" }]);
+    t.deepEqual(service.search("aver"), {
+        matches: [
+            { index: 0, match: "haber", post: "", pre: "", source: "haber", weight: 50 }
+        ],
+        maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service should weed out duplicate phonetic matches", (t) => {
+    let service = new VerbSearchService([{
+        name: "Atar",
+        indicative: { present: [["ato"],["atas"],["ata"]] },
+        subjunctive: { present: [["ate"],["ates"],["ate"]] },
+        imperative: { affirmative: [[],["ata"],["ate"]] }
+    }]);
+    t.deepEqual(service.search("ato"), {
+        matches: [
+            { index: 0, match: "ato", post: "", pre: "", source: "atar", weight: 59 },
+            { index: 0, match: "ata", post: "", pre: "", source: "atar", weight: 49 },
+            { index: 0, match: "ate", post: "", pre: "", source: "atar", weight: 49 } ],
+        maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service should not exceed max search results when matching from beginning", (t) => {
+    let service = new VerbSearchService([
+        { name: "atestiguar" },
+        { name: "atacar" },
+        { name: "atender" },
+        { name: "aterrizar" },
+        { name: "aterrorizar" },
+        { name: "atar" },
+        { name: "atraer" },
+        { name: "atrapar" },
+        { name: "atravesar" },
+        { name: "atribuir" },
+        { name: "atropellar" }
+    ]);
+
+    t.deepEqual(service.search("at").matches.length, 10);
+    t.end();
+});
+
+test("Verb search service should not exceed max search results when matching inside", (t) => {
+    let service = new VerbSearchService([
+        { name: "acostar" },
+        { name: "ajustar" },
+        { name: "apostar" },
+        { name: "asustar" },
+        { name: "arrestar" },
+        { name: "bastar" },
+        { name: "conquistar" },
+        { name: "contestar" },
+        { name: "costar" },
+        { name: "estar" },
+        { name: "detestar" }
+    ]);
+
+    t.deepEqual(service.search("star").matches.length, 10);
+    t.end();
+});
+
+test("Verb search service should order descending by weight", (t) => {
+    let service = new VerbSearchService([
+        { name: "dormir" },
+        { name: "gemir" },
+        { name: "mirar" }
+    ]);
+
+    t.deepEqual(service.search("mir"), {
+        matches: [
+            { index: 2, match: "mir", post: "ar", pre: "", source: "", weight: 90 },
+            { index: 1, match: "mir", post: "", pre: "ge", source: "", weight: 78 },
+            { index: 0, match: "mir", post: "", pre: "dor", source: "", weight: 77 }
+        ],
+        maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service create phonetic index from data with objects and arrays", (t) => {
+    let service = new VerbSearchService([
+        { name: "ir", indicative: { present: [["voy"]] } }
+    ]);
+
+    t.deepEqual(service.search("voy"), {
+        matches: [
+            { index: 0, match: "voy", post: "", pre: "", source: "ir", weight: 59 }
+        ],
+        maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service create phonetic index from data with objects and arrays and ignore unknown data types", (t) => {
+    let service = new VerbSearchService([
+        { name: "ir", unknown: 123, indicative: { present: [["voy"]] } }
+    ]);
+
+    t.deepEqual(service.search("voy"), {
+        matches: [
+            { index: 0, match: "voy", post: "", pre: "", source: "ir", weight: 59 }
+        ],
+        maxExceeded: false });
+    t.end();
+});
+
+test("Verb search service create phonetic index lazily", (t) => {
+    let service = new VerbSearchService([
+        { name: "ir", indicative: { present: [["voy"]] } }
+    ]);
+
+    t.true(service.phoneticIndex === undefined);
+    service.search("voy");
+    t.true(service.phoneticIndex !== undefined);
+
+    let index = service.phoneticIndex;
+    service.search("voy");
+
+    t.equal(service.phoneticIndex, index);
     t.end();
 });

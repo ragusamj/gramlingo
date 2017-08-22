0b94a8667ab7a77a394ad172ba831ae203b0cb34
erikbarke <e@barke.nu>
2017-08-22 06:27:33 +0200

    Randomizing unique values

--- src/app/pages/numerals/generator.js
+++ src/app/pages/numerals/generator.js
@@ -13,6 +13,9 @@ class Generator {
             time: this.randomizeTime.bind(this),
         };
         this.lastCenturiesValues = [];
+        this.lastFractionValues = [];
+        this.lastIntegerValues = [];
+        this.lastTimeValues = [];
     }
 
     all() {
@@ -36,11 +39,14 @@ class Generator {
             let century = this.getRandomNumber(0, 29) * 100;
             if(buffer.indexOf(century) === -1 && this.lastCenturiesValues.indexOf(century) === -1) {
                 buffer.push(century);
-                result.push({
-                    q: [[century + "-" + (century + 99)]],
-                    a: [[Century.spell(century)]]
-                });
             }
+        }
+
+        for (let century of buffer.sort((a, b) => (a - b))) {
+            result.push({
+                q: [[century + "-" + (century + 99)]],
+                a: [[Century.spell(century)]]
+            });
         }
 
         this.lastCenturiesValues = buffer;
@@ -51,106 +57,87 @@ class Generator {
     randomizeFractions() {
         
         let result = [];
-        let fractions = [
-            [this.getRandomNumber(1, 5), this.getRandomNumber(2, 5)],
-            [this.getRandomNumber(1, 5), this.getRandomNumber(2, 5)],
-            [this.getRandomNumber(1, 5), this.getRandomNumber(6, 10)],
-            [this.getRandomNumber(1, 10), this.getRandomNumber(6, 10)],
-            [this.getRandomNumber(1, 10), this.getRandomNumber(2, 5)],
-            [this.getRandomNumber(1, 10), this.getRandomNumber(11, 30)],
-            [this.getRandomNumber(11, 100), this.getRandomNumber(31, 100)]
-        ];
-
-        this.shuffleArray(fractions);
+        let buffer = [];
         
-        for (let fraction of fractions) {
-            result.push({
-                q: [[fraction[0] + "/" + fraction[1]]],
-                a: [Fraction.spell(fraction[0], fraction[1])]
-            });
+        while(buffer.length < 6) {
+            let numerator = this.getRandomNumber(1, 10);
+            let denominator = this.getRandomNumber(2, 10);
+            let fraction = numerator + "/" + denominator;
+            if(buffer.indexOf(fraction) === -1 && this.lastFractionValues.indexOf(fraction) === -1) {
+                buffer.push(fraction);
+                result.push({
+                    p: numerator / denominator,
+                    q: [[fraction]],
+                    a: [Fraction.spell(numerator, denominator)]
+                });
+            }
         }
+
+        this.lastCenturiesValues = buffer;
         
-        return result;
+        return result.sort((a, b) => a.p - b.p);
     }
 
     randomizeIntegers() {
 
         let result = [];
+        let buffer = [];
         let now = new Date();
-        let integers = [
-            this.getRandomNumber(1, 29),
-            this.getRandomNumber(30, 100),
-            this.getRandomNumber(100, 1000),
-            this.getRandomNumber(now.getFullYear() - 300, now.getFullYear() + 50),
-            this.getRandomNumber(10000, 100000),
-            this.getRandomNumber(100000, 1000000)
+        let ranges = [
+            [1, 29],
+            [30, 100],
+            [100, 1000],
+            [now.getFullYear() - 300, now.getFullYear() + 50],
+            [10000, 100000],
+            [100000, 1000000]
         ];
 
-        this.shuffleArray(integers);
-
-        for (let integer of integers) {
+        for (let range of ranges) {
+            let integer;
+            do {
+                integer = this.getRandomNumber(range[0], range[1]);
+            }
+            while(this.lastIntegerValues.indexOf(integer) !== -1);
+            buffer.push(integer);
             result.push({
                 q: [[integer]],
                 a: [[Integer.spell(integer)]]
             });
         }
-
+    
+        this.lastIntegerValues = buffer;
         return result;
     }
 
     randomizeTime() {
-        
-        let result = [];
-        let times = [
-            [this.getRandomNumber(0, 23), this.getRandomNumber(0, 59)],
-            [this.getRandomNumber(0, 23), this.getRandomNumber(0, 59)],
-            [this.getRandomNumber(0, 23), this.getRandomNumber(0, 59)],
-            [this.getRandomNumber(0, 23), this.getRandomNumber(0, 59)],
-            [this.getRandomNumber(0, 23), this.getRandomNumber(0, 59)],
-            [this.getRandomNumber(0, 23), this.getRandomNumber(0, 59)]
-        ];
 
-        this.shuffleArray(times);
-        
-        for (let time of times) {
-            result.push({
-                q: [[this.formatTimeSpan(time[0], time[1])]],
-                a: [Time.spell(time[0], time[1])]
-            });
+        let result = [];
+        let buffer = [];
+
+        while(buffer.length < 6) {
+            let hour = this.getRandomNumber(0, 23);
+            let minute = this.getRandomNumber(0, 59);
+            let time = this.formatTimeSpan(hour, minute);
+            if(buffer.indexOf(time) === -1 && this.lastTimeValues.indexOf(time) === -1) {
+                buffer.push(time);
+                result.push({
+                    q: [[time]],
+                    a: [Time.spell(hour, minute)]
+                });
+            }
         }
+
+        this.lastTimeValues = buffer;
         
-        return result;
+        return result.sort((a, b) => a.q[0][0].localeCompare(b.q[0][0]));
     }
 
     getRandomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    shuffleArray(array) {
-        // Fisher-Yates Shuffle
-        let i = array.length;
-        if (array.length > 0) {
-            while (--i) {
-                let j = Math.floor(Math.random() * (i + 1));
-                let tempi = array[i];
-                let tempj = array[j];
-                array[i] = tempj;
-                array[j] = tempi;
-            }
-        }
-    }
-
-    padLeft(value, length, padchar) {
-        let result = value.toString();
-        let pad;
-        for (pad = length - result.length; pad > 0; pad--) {
-            result = padchar + result;
-        }
-        return result;
-    }
-        
     formatTimeSpan(hour, minute) {
-        return this.padLeft(hour, 2, "0") + ":" + this.padLeft(minute, 2, "0");
+        return (hour < 10 ? "0" : "") + hour + ":" + (minute < 10 ? "0" : "") + minute;
     }
 }
 

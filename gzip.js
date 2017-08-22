f2be68dcfe0e0a0516b14974d4c3256e8affac78
erikbarke <e@barke.nu>
2017-08-22 21:26:45 +0200

    Log when done

--- gzip.js
+++ gzip.js
@@ -1,5 +1,6 @@
 let fs = require("fs");
 let glob = require("glob");
+let path = require("path");
 let zlib = require("zlib");
 
 let compressionLevel = zlib.Z_BEST_COMPRESSION || zlib.constants.Z_BEST_COMPRESSION;
@@ -9,7 +10,7 @@ glob(process.argv[2], {}, function (error, files) {
         fs.readFile(file, function(error, data) {
             zlib.gzip(data, { level: compressionLevel }, function(error, buffer) {
                 fs.writeFile(file + ".gz", buffer, function() {
-                    //
+                    process.stdout.write(path.basename(__filename) + ": done.");
                 });
             });
         });

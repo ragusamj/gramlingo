e58cd221bc7cf63fec56a65f97acd7459fc3c94a
erikbarke <e@barke.nu>
2017-08-30 07:14:57 +0200

    Sanitizer, not Checker

--- src/app/pages/common/sanitizer.spec.js
+++ src/app/pages/common/sanitizer.spec.js
@@ -1,22 +1,22 @@
 import test from "tape";
 import Sanitizer from "./sanitizer";
 
-test("Checker should sanitize string and only leave spanish characters", (t) => {
+test("Sanitizer should sanitize string and only leave spanish characters", (t) => {
     t.equal(Sanitizer.sanitize("#&abc123ÁáÉéÍíÓóÚúüÑñªº..,û"), "abc123ÁáÉéÍíÓóÚúüÑñªº");
     t.end();
 });
 
-test("Checker should sanitize string and trim multiple whitespaces", (t) => {
+test("Sanitizer should sanitize string and trim multiple whitespaces", (t) => {
     t.equal(Sanitizer.sanitize("abc    123"), "abc 123");
     t.end();
 });
 
-test("Checker should sanitize string and leave :", (t) => {
+test("Sanitizer should sanitize string and leave :", (t) => {
     t.equal(Sanitizer.sanitize("12:34"), "12:34");
     t.end();
 });
 
-test("Checker should sanitize string and leave /", (t) => {
+test("Sanitizer should sanitize string and leave /", (t) => {
     t.equal(Sanitizer.sanitize("3/4"), "3/4");
     t.end();
 });

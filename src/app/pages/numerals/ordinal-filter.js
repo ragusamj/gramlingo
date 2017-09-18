b6e9e4cc64f4c2c239cb6d86cad5646e298c14c7
erikbarke <e@barke.nu>
2017-09-18 09:24:37 +0200

    Using string.endsWith

--- src/app/pages/numerals/ordinal-filter.js
+++ src/app/pages/numerals/ordinal-filter.js
@@ -1,7 +1,7 @@
 export default (element, solutions) => {
     if(element.value) {
         for(let sign of ["ª", "º"]) {
-            if(solutions[0].indexOf(sign) === (solutions[0].length) -1 && element.value.indexOf(sign) === -1) {
+            if(solutions[0].endsWith(sign) && !element.value.endsWith(sign)) {
                 element.value += sign;
             }
         }

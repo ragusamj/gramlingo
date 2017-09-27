01f5bef2612a4157a7ba0b8e02a927a96eabe423
erikbarke <e@barke.nu>
2017-09-27 06:40:57 +0200

    Project structure, map and flags

--- src/generators/country-flags/flags.js
+++ src/generators/country-flags/flags.js
@@ -2,14 +2,15 @@ const fs = require("pn/fs");
 const path = require("path");
 const svg2png = require("svg2png");
 
-const root = "../../node_modules/flag-icon-css/flags/4x3/";
+const root = path.join(__dirname, "../../../node_modules/flag-icon-css/flags/4x3");
+const target = path.join(__dirname, "../../images/flags");
 
 fs.readdir(root)
     .then((files) => {
         for(let file of files) {
             fs.readFile(path.join(root, file))
                 .then(sourceBuffer => svg2png(sourceBuffer, { width: 100 }))
-                .then(buffer => fs.writeFile(path.join("./flags/", path.basename(file, ".svg").toUpperCase() + ".png"), buffer))
+                .then(buffer => fs.writeFile(path.join(target, path.basename(file, ".svg").toUpperCase() + ".png"), buffer))
                 .catch(e => process.stderr.write(e));
         }
     });

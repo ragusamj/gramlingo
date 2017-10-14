a4d6a3faaa2cb2208754d78377c7b495b06002a0
erikbarke <e@barke.nu>
2017-10-14 09:33:18 +0200

    Removed the click listener

--- src/app/pages/world/world-map/world-map-listener.js
+++ src/app/pages/world/world-map/world-map-listener.js
@@ -12,7 +12,6 @@ class WorldMapListener {
 
     attach() {
         this.removeListeners = [
-            this.browserEvent.on("click", this.worldMap.onClick.bind(this.worldMap)),
             this.browserEvent.on("mousedown", this.worldMap.onMousedown.bind(this.worldMap)),
             this.browserEvent.on("mousemove", this.worldMap.onMousemove.bind(this.worldMap)),
             this.browserEvent.on("mouseup", this.worldMap.onMouseup.bind(this.worldMap)),

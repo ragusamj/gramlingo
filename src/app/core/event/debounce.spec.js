b73f7268e46a933cb8944952578e40e234f6e4bd
erikbarke <e@barke.nu>
2017-10-24 06:29:06 +0200

    Added debounce test

--- src/app/core/event/debounce.spec.js
+++ src/app/core/event/debounce.spec.js
@@ -2,6 +2,36 @@ import sinon from "sinon";
 import test from "tape";
 import debounce from "./debounce";
 
+test("Debounce should debounce and wait for n ms and then run once", function(t) {
+
+    let counter = 0;
+    let clock = sinon.useFakeTimers();
+    let debounced = debounce(function() {
+        counter++;
+    }, 100);
+  
+    debounced();
+    clock.tick(50);
+
+    debounced();
+    debounced();
+    clock.tick(50);
+
+    debounced();
+    clock.tick(50);
+
+    debounced();
+    debounced();
+    debounced();
+    clock.tick(50);
+
+    debounced();
+    clock.tick(300);
+  
+    t.equal(counter, 1);
+    t.end();
+});
+
 test("Debounce should debounce and execute the callback with arguments", (t) => {
     t.plan(1);
 

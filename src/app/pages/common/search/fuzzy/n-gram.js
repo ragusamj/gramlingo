eb065df433ddbd5de6c3c972261383927eb4618f
erikbarke <e@barke.nu>
2017-10-25 06:41:39 +0200

    Fix for one character values

--- src/app/pages/common/search/fuzzy/n-gram.js
+++ src/app/pages/common/search/fuzzy/n-gram.js
@@ -1,5 +1,5 @@
 export default (value, n) => {
-    let grams = [];
+    let grams = value.length === 1 ? [value] : [];
     for(let i = 0; i < value.length - 1; i++){
         grams[i] = value.slice(i, i + n);
     }
--- src/app/pages/common/search/fuzzy/n-gram.spec.js
+++ src/app/pages/common/search/fuzzy/n-gram.spec.js
@@ -6,6 +6,11 @@ test("n-gram should create a bigram", (t) => {
     t.end();
 });
 
+test("n-gram should create a bigram, one character", (t) => {
+    t.deepEqual(ngram("2", 2), ["2"]);
+    t.end();
+});
+
 test("n-gram should create a trigram", (t) => {
     t.deepEqual(ngram("TRIGRAM", 3), ["TRI", "RIG", "IGR", "GRA", "RAM", "AM"]);
     t.end();

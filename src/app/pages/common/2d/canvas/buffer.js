766214c21290652b563b8cfccfb0acf30ca9c122
erikbarke <e@barke.nu>
2017-11-24 06:50:01 +0100

    Moved triangulation to separate class

---
+++ src/app/pages/common/2d/canvas/buffer.js
@@ -0,0 +1,53 @@
+import earcut from "earcut";
+import Color from "../../color";
+
+class Buffer {
+
+    static create(geometries) {
+        const buffer = { data: [], byColor: {} };
+        const geometriesByColor = this.sortByColor(geometries);
+
+        this.bufferByColor = {};
+        
+        for(let color of Object.keys(geometriesByColor)) {
+            const offset = buffer.data.length;
+            for(let geometry of geometriesByColor[color]) {
+                if(geometry.type === "Polygon") {
+                    this.triangulate(geometry.polygons, buffer.data);
+                }
+                if(geometry.type === "MultiPolygon") {
+                    for(let polygons of geometry.polygons) {
+                        this.triangulate(polygons, buffer.data);
+                    }
+                }
+            }
+            buffer.byColor[color] = buffer.byColor[color] || {
+                vec4: Color.vec4(color),
+                length: (buffer.data.length - offset) / 2,
+                offset: offset / 2
+            };
+        }
+
+        return buffer;
+    }
+
+    static sortByColor(geometries) {
+        const sorted = {};
+        for(let geometry of geometries) {
+            sorted[geometry.color] = sorted[geometry.color] || [];
+            sorted[geometry.color].push(geometry);
+        }
+        return sorted;
+    }
+
+    static triangulate(polygons, data) {
+        let points = earcut.flatten(polygons);
+        let triangles = earcut(points.vertices, points.holes, points.dimensions);
+        for(let t of triangles) {
+            let i = t * 2;
+            data.push(points.vertices[i], points.vertices[i + 1]);
+        }
+    }
+}
+
+export default Buffer;
